a8f024fe529b142f63763d9ee8797193e5f830fc
nomancsediu <149802233+Noman-797@users.noreply.github.com>
2025-11-22 23:23:43 +0600

    Clean repository: Keep only Day - 1 LeetCode solution

--- AIAGENT/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ChatMessage
-
-@admin.register(ChatMessage)
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ['user_message', 'timestamp']
-    list_filter = ['timestamp']
-    readonly_fields = ['timestamp']
